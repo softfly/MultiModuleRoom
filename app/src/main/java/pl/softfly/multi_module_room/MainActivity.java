43085c8a883a73996aad02c473d6987e08c98916
Grzegorz Ziemski <ziemski@gmail.com>
2023-08-14 20:05:46 +0100

    Initialize the project from the IntelliJ generator

---
+++ app/src/main/java/pl/softfly/multi_module_room/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.softfly.multi_module_room;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
